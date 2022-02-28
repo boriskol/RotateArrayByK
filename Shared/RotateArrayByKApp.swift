89715d9bcc8ce0622d7a6473d06e47d08b3ac882
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-02-28 08:38:22 +0100

    Initial Commit

---
+++ Shared/RotateArrayByKApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotateArrayByKApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 28/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RotateArrayByKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
